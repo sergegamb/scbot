84ac2413acdeaad98ee53e73eeb93d2676cbc287
Sergey Gambaryan <serge_gamb@yahoo.com>
2025-02-04 15:02:36 +0300

    Unindent

--- filters.py
+++ filters.py
@@ -21,68 +21,69 @@ TECHNICIANS = {
 }
 
 async def filter_action(update: Update, _):
-        logger.info("Choose filter view")
-        await update.callback_query.answer("Which one?")
-        await update.callback_query.edit_message_text(
-                text="Choose a filter",
-                reply_markup=InlineKeyboardMarkup.from_column(
-                        [
-                                InlineKeyboardButton(
-                                        text="All requests",
-                                        callback_data="filter_all"
-                                ),
-                                InlineKeyboardButton(
-                                        text="My requests",
-                                        callback_data="filter_my"
-                                ),
-                                InlineKeyboardButton(
-                                        text="All my groups",
-                                        callback_data="filter_all_my_groups"
-                                )
-                        ]
+    logger.info("Choose filter view")
+    await update.callback_query.answer("Which one?")
+    await update.callback_query.edit_message_text(
+        text="Choose a filter",
+        reply_markup=InlineKeyboardMarkup.from_column(
+            [
+                InlineKeyboardButton(
+                    text="All requests",
+                    callback_data="filter_all"
+                ),
+                InlineKeyboardButton(
+                    text="My requests",
+                    callback_data="filter_my"
+                ),
+                InlineKeyboardButton(
+                    text="All my groups",
+                    callback_data="filter_all_my_groups"
                 )
+            ]
         )
-        return PICK
+    )
+    return PICK
 
 
 # TODO: use function request_by_filter
 async def pick_filter_action(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        chosen_filter = update.callback_query.data
-        technician = TECHNICIANS[update.callback_query.from_user.id]
-        if chosen_filter == "filter_all":
-                logger.info("Filter all was chosen")
-                await update.callback_query.answer("All requests")
-                context.user_data["filter"] = "all"
-                requests = RequestInterface.list_all(0)
-        elif chosen_filter.startswith("filter_my"):
-                logger.info("Filter my was chosen")
-                await update.callback_query.answer("Your requests")
-                context.user_data["filter"] = "my"
-                requests = RequestInterface.list_technician(0, technician)
-        else: # chosen_filter.startswith("filter_all_my_groups"):
-                logger.info("Filter my all groups was chosen")
-                await update.callback_query.answer("All your groups")
-                context.user_data["filter"] = "all_my_groups"
-                requests = RequestInterface.list_technician_group(0, technician)
-        context.user_data["page"] = 0
-        await request_list_view(update, context, requests)
-        return ConversationHandler.END
+    chosen_filter = update.callback_query.data
+    technician = TECHNICIANS[update.callback_query.from_user.id]
+    logging.info(technician)
+    if chosen_filter == "filter_all":
+        logger.info("Filter all was chosen")
+        await update.callback_query.answer("All requests")
+        context.user_data["filter"] = "all"
+        requests = RequestInterface.list_all(0)
+    elif chosen_filter.startswith("filter_my"):
+        logger.info("Filter my was chosen")
+        await update.callback_query.answer("Your requests")
+        context.user_data["filter"] = "my"
+        requests = RequestInterface.list_technician(0, technician)
+    else: # chosen_filter.startswith("filter_all_my_groups"):
+        logger.info("Filter my all groups was chosen")
+        await update.callback_query.answer("All your groups")
+        context.user_data["filter"] = "all_my_groups"
+        requests = RequestInterface.list_technician_group(0, technician)
+    context.user_data["page"] = 0
+    await request_list_view(update, context, requests)
+    return ConversationHandler.END
 
 filters_handler = ConversationHandler(
-        entry_points=[
-                CallbackQueryHandler(
-                        filter_action,
-                        "filters"
-                )
-        ],
-        states={
-                PICK: [
-                        CallbackQueryHandler(
-                                pick_filter_action
-                        )
-                ]
-        },
-        fallbacks=[],
-        per_message=False,
+    entry_points=[
+        CallbackQueryHandler(
+            filter_action,
+            "filters"
+        )
+    ],
+    states={
+        PICK: [
+            CallbackQueryHandler(
+                pick_filter_action
+            )
+        ]
+    },
+    fallbacks=[],
+    per_message=False,
 )
 
